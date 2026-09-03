24ca05eb00509429ca91483841b5643c96df7b25
Lewiston Courier Service <76872929+TheUnemployer@users.noreply.github.com>
2026-09-03 13:23:53 -0400

    Route retail pickup requests into dispatch

--- worker-entry.js
+++ worker-entry.js
@@ -1,8 +1,14 @@
 import baseWorker from "./worker.js";
 import { handleSquareRestaurantConnector } from "./square-restaurant-connector.js";
+import { handleRetailPickup } from "./retail-pickup.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const retailResponse = await handleRetailPickup(request, env, ctx);
+    if (retailResponse) {
+      return retailResponse;
+    }
+
     const connectorResponse = await handleSquareRestaurantConnector(
       request,
       env,
