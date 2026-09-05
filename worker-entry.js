dc1af93caadb950b3f9b0264fb0d225b55d29a18
Lewiston Courier Service <76872929+TheUnemployer@users.noreply.github.com>
2026-09-04 22:09:26 -0400

    Wire marketplace order router into Worker entry

--- worker-entry.js
+++ worker-entry.js
@@ -2,9 +2,15 @@ import baseWorker from "./worker.js";
 import { handleSquareRestaurantConnector } from "./square-restaurant-connector.js";
 import { handleSquareMenuSync } from "./square-menu-sync.js";
 import { handleRetailPickup } from "./retail-pickup.js";
+import { handleMarketplaceOrders } from "./marketplace-orders.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const marketplaceResponse = await handleMarketplaceOrders(request, env, ctx);
+    if (marketplaceResponse) {
+      return marketplaceResponse;
+    }
+
     const retailResponse = await handleRetailPickup(request, env, ctx);
     if (retailResponse) {
       return retailResponse;
